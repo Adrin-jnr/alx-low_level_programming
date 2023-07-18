570011981bc77db1383050fe975f11ddd0b33f99
Adrin-jnr <adrinjnr@gmail.com>
2023-07-18 08:13:35 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,53 +1,51 @@
 #include "main.h"
 
-#include <stdio.h>
-
 /**
- * print_times_table - Prints the n times table starting from 0
- * @n: The value of n
- */
-
-void print_ times_table(int n)
+  * print_times_table - Prints a multiplication table up to param
+  * @n: The number to be treated
+  *
+  * Return: Number matrix
+  */
+void print_times_table(int n)
 {
-	if (n > 15 || n < 0)
-		return; (times_table)
+	int x, y, z;
 
-	int i, j * result;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 14)
 	{
-		for (j = 0; j <= n; j++)
+		for (x = 0; x <= n; x++)
 		{
-			result = i * j;
-			if (j != 0)
+			for (y = 0; y <= n; y++)
 			{
-				_putchar(' ');
-				if (result <= 9)
-					_putchar(' ');
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
 			}
-			if (result > 99)
-			{
-				_putchar((result / 100) + '0');
-				_putchar(((result / 10) % 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-			else if (result > 9)
-			{
-				_putchar(' ');
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + '0');
-			}
-
-			if (j != n)
-				_putchar(',');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
-
